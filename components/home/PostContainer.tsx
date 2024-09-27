954a94c2df5917894bdc18a8b0580b51527ac2ce
mahshid22 <mahshid.pouyafar@gmail.com>
2024-09-27 20:29:07 +0330

    add viewabilityConfig

--- components/home/PostContainer.tsx
+++ components/home/PostContainer.tsx
@@ -1,18 +1,37 @@
 import { View, Text, FlatList } from "react-native";
-import React from "react";
+import React, { useRef, useState } from "react";
 import Posts from "@/constants/posts";
 import PostCard from "./PostCard";
 
 const PostContainer = () => {
+  const [visibleVideoIndex, setVisibleVideoIndex] = useState<number | null>(
+    null,
+  );
+  const onViewableItemsChanged = useRef(({ viewableItems }: any) => {
+    const index = viewableItems[0].index;
+    setVisibleVideoIndex(index);
+  });
+
   return (
     <FlatList
-      data={Posts}
-      renderItem={({ item, index }) => <PostCard  item={item} index={index} />}
-      scrollEnabled={false}
-      pagingEnabled
-      contentContainerStyle={{ gap: 16 }}
-      keyExtractor={(item)=> item.id.toString()}
-    />
+        data={Posts}
+        keyExtractor={(item) => item.id.toString()}
+        scrollEnabled={false}
+        pagingEnabled
+        onViewableItemsChanged={onViewableItemsChanged.current}
+        viewabilityConfig={{ itemVisiblePercentThreshold: 50 }}
+        contentContainerStyle={{
+          gap: 16,
+        }}
+        renderItem={({ item, index }) => (
+          <PostCard
+            item={item}
+            index={index}
+            visibleVideoIndex={visibleVideoIndex}
+            // handleModal={handleOpenPress}
+          />
+        )}
+      />
   );
 };
 
